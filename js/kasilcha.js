6d48a3a0c36068ce7f5fb71b861a85f5e6eb3d1d
madhusanka.goonathilake <madhusanka.goonathilake@gmail.com>
2012-09-24 13:34:36 +0000

    Added status based messages with infrastructure to include more assertions

--- js/kasilcha.js
+++ js/kasilcha.js
@@ -1,38 +1,120 @@
 var Kasilcha = {
-    tests: [],
-    add: function(assertion) {
-        this.tests.push(assertion);
+    
+    config: {
+        'show-success-mode': false,
+        'show-error-mode': false
     },
+    tests: [],
+    
+    set: function(key, value) {
+        this.config[key] = value;
+    },
+    
+    is: function(key) {
+        return (this.config[key] === true);
+    },
+    
+    add: function(assertionComponent) {
+        this.tests.push(assertionComponent);
+    },
+    
+    tag: function(tag, attributes) {
+        var element = $('<' + tag + '/>');
+        
+        if (attributes !== undefined) {
+            element.attr(attributes);
+        }
+        
+        return element;
+    },
+    
+    content_tag: function(tag, content, attributes) {
+        var element = $('<' + tag + '>' + content + '</' + tag + '>');
+        
+        if (attributes !== undefined) {
+            element.attr(attributes);
+        }
+        
+        return element;
+    },
+    
+    log: function(message, type, description) {
+        $('#console').append(this.content_tag('span', message, {
+            'class': type
+        })).append(this.tag('br'));
+        
+        if (description !== undefined) {
+            $('#console span:last').append(this.content_tag('span', description, {
+                'class': 'description'
+            }).hide()).addClass('contains-description').hover(function() {
+                $(this).children().show();
+            }, function() {
+                $(this).children().hide();
+            }).append(this.tag('br'));
+        }
+    },
+
     run: function() {
         var i, result, total = 0, success = 0, failure = 0;
         
         for (i in this.tests) {
             
-            result = this.tests[i].run();
+            result = this.tests[i].run(this);
             
             if (result === true) {
                 success++;
-            } else {
+                total++;
+            } else if (result === false) {
                 failure++;
+                total++;
             }
             
-            total++;
+            
         }
         
         var percentage = Math.floor((success / total) * 100);
         
-        $('#console').prepend('<span class="info">' + total + ' Tests, ' + success + ' Passed, ' + failure +' Failed.</span><br />');
+        var summary = total + ' Tests, ' + success + ' Passed, ' + failure +' Failed (' + percentage + '%)';
+        var summaryType = (percentage === 100) ? 'success' : 'warning';
+        
+        $('#summary').append(this.content_tag('span', summary, {
+            'class': summaryType
+        })).append(this.tag('br'));
         
     },
-    assertEquals: function(expected, actual, message) {
+
+    assertArrayHasKey: function (key, array, description) {
         var assertion = {
-            run: function() {
-                if (expected !== actual) {
-                    if (message === undefined) {
-                        message = 'Failed asserting ' + actual + ' equals the expected value ' + expected;
+            run: function(context) {
+                var index;
+                for (index in array) {
+                    if (index == key) {
+                        if (context.is('show-success-mode')) {
+                            context.log('Asserted that ' + key + ' exists in array', 'success');
+                        }
+                        return true;
                     }
-                    $('#console').append('<span class="failure">' + message + '</span><br />');
+                }
+                
+                context.log('Failed to assert that ' + key + ' exists in array', 'failure', description);
+                
+                return false;
+            }
+        };
+        
+        this.add(assertion);
+    },
+
+    assertEquals: function(expected, actual, description) {
+        var assertion = {
+            run: function(context) {
+                if (expected !== actual) {
+                    context.log('Failed asserting ' + actual + ' equals the expected value ' + expected, 'failure', description);
                     return false;
+                }
+                
+                if (context.is('show-success-mode')) {
+                    context.log('Asserted that ' + actual + ' is equal to ' + expected, 'success');
                 }
                 
                 return true;
