6415873cf4a3aa60ca867b4df8e0337c586f73c7
madhusanka.goonathilake@gmail.com <madhusanka.goonathilake@gmail.com@e1e926d5-f571-e2bc-2335-723a295f3548>
2012-09-24 13:34:36 +0000

    Added status based messages with infrastructure to include more assertions

--- trunk/js/tests.js
+++ trunk/js/tests.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+
     with (Kasilcha) {
-            add(equals(5, 5));
-            add(equals(5, 6));
-            add(equals('abc', 'abc'));
-            add(equals('abc', 'AbC'));
-            add(equals('abc', 'pqr'));
-            
-            run();
+        set('show-success-mode', true);
+        
+        assertEquals(1, 1);
+        assertEquals(1, 2, 'This is a sample test, meant to be failed.');
+        
+        assertArrayHasKey(1, ['a', 'b', 'c']);
+        assertArrayHasKey('b', { a: 1, b: 2, c: 3});
+        assertArrayHasKey(10, ['a', 'b', 'c'], 'This test is also meant to be failed');
+        assertArrayHasKey('p', { a: 1, b: 2, c: 3});
+        run();
     }
 });
