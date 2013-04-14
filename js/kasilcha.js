ff72f8df9faa23874fb85a0a7f5f044af92160f3
madhusanka.goonathilake <madhusanka.goonathilake@gmail.com>
2013-04-14 06:17:01 +0000

    Added assertGreaterThan() assertion method

--- js/kasilcha.js
+++ js/kasilcha.js
@@ -1,48 +1,41 @@
 var Kasilcha = {
-    
     config: {
         'show-success-mode': false,
         'show-error-mode': false
     },
     tests: [],
-    
     set: function(key, value) {
         this.config[key] = value;
     },
-    
     is: function(key) {
         return (this.config[key] === true);
     },
-    
     add: function(assertionComponent) {
         this.tests.push(assertionComponent);
     },
-    
     tag: function(tag, attributes) {
         var element = $('<' + tag + '/>');
-        
+
         if (attributes !== undefined) {
             element.attr(attributes);
         }
-        
+
         return element;
     },
-    
     content_tag: function(tag, content, attributes) {
         var element = $('<' + tag + '>' + content + '</' + tag + '>');
-        
+
         if (attributes !== undefined) {
             element.attr(attributes);
         }
-        
+
         return element;
     },
-    
     log: function(message, type, description) {
         $('#console').append(this.content_tag('span', message, {
             'class': type
         })).append(this.tag('br'));
-        
+
         if (description !== undefined) {
             $('#console span:last').append(this.content_tag('span', description, {
                 'class': 'description'
@@ -53,14 +46,13 @@ var Kasilcha = {
             }).append(this.tag('br'));
         }
     },
-
     run: function() {
         var i, result, total = 0, success = 0, failure = 0;
-        
+
         for (i in this.tests) {
-            
+
             result = this.tests[i].run(this);
-            
+
             if (result === true) {
                 success++;
                 total++;
@@ -68,22 +60,20 @@ var Kasilcha = {
                 failure++;
                 total++;
             }
-            
-            
+
         }
-        
+
         var percentage = Math.floor((success / total) * 100);
-        
-        var summary = total + ' Tests, ' + success + ' Passed, ' + failure +' Failed (' + percentage + '%)';
+
+        var summary = total + ' Tests, ' + success + ' Passed, ' + failure + ' Failed (' + percentage + '%)';
         var summaryType = (percentage === 100) ? 'success' : 'warning';
-        
+
         $('#summary').append(this.content_tag('span', summary, {
             'class': summaryType
         })).append(this.tag('br'));
-        
-    },
 
-    assertArrayHasKey: function (key, array, description) {
+    },
+    assertArrayHasKey: function(key, array, description) {
         var assertion = {
             run: function(context) {
                 var index;
@@ -95,16 +85,15 @@ var Kasilcha = {
                         return true;
                     }
                 }
-                
+
                 context.log('Failed to assert that ' + key + ' exists in array', 'failure', description);
-                
+
                 return false;
             }
         };
-        
+
         this.add(assertion);
     },
-    
     assertEmpty: function(value, description) {
         var assertion = {
             run: function(context) {
@@ -112,18 +101,17 @@ var Kasilcha = {
                     context.log('Failed to assert that ' + value + ' is empty', 'failure', description);
                     return false;
                 }
-                
+
                 if (context.is('show-success-mode')) {
                     context.log('Asserted that ' + value + ' is empty', 'success');
                 }
-                
+
                 return true;
             }
         };
-        
+
         this.add(assertion);
     },
-
     assertEquals: function(expected, actual, description) {
         var assertion = {
             run: function(context) {
@@ -131,18 +119,17 @@ var Kasilcha = {
                     context.log('Failed asserting ' + actual + ' equals the expected value ' + expected, 'failure', description);
                     return false;
                 }
-                
+
                 if (context.is('show-success-mode')) {
                     context.log('Asserted that ' + actual + ' is equal to ' + expected, 'success');
                 }
-                
+
                 return true;
             }
         }
-        
+
         this.add(assertion);
     },
-
     assertTrue: function(value, description) {
         var assertion = {
             run: function(context) {
@@ -150,19 +137,17 @@ var Kasilcha = {
                     if (context.is('show-success-mode')) {
                         context.log('Asserted that ' + value + ' is true', 'success');
                     }
-                    
+
                     return true;
                 }
-                
-                
+
                 context.log('Failed asserting ' + value + ' is true', 'failure', description);
                 return false;
             }
         }
-        
+
         this.add(assertion);
     },
-
     assertFalse: function(value, description) {
         var assertion = {
             run: function(context) {
@@ -170,15 +155,33 @@ var Kasilcha = {
                     if (context.is('show-success-mode')) {
                         context.log('Asserted that ' + value + ' is false', 'success');
                     }
-                    
+
                     return true;
                 }
-                
+
                 context.log('Failed asserting ' + value + ' is false', 'failure', description);
                 return false;
             }
         }
-        
+
+        this.add(assertion);
+    },
+    assertGreaterThan: function(compareTo, value, description) {
+        var assertion = {
+            run: function(context) {
+                if (value > compareTo) {
+                    if (context.is('show-success-mode')) {
+                        context.log('Asserted that ' + value + ' is greater than ' + compareTo, 'success');
+                    }
+
+                    return true;
+                }
+
+                context.log('Failed asserting that ' + value + ' is greater than ' + compareTo, 'failure', description);
+                return false;
+            }
+        }
+
         this.add(assertion);
     }
 };
